2b254e7479237c2a8984f9f132ff4a193b147a70
awaco <awaconteh95@gmail.com>
2023-03-05 16:16:45 -0500

    updated cukesRunner

--- src/test/java/com/cydeo/runners/cukesRunner.java
+++ src/test/java/com/cydeo/runners/cukesRunner.java
@@ -7,17 +7,18 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions (
         plugin = {
-
+                "json:target/cucumber.json",
                 "html:target/cucumber-reports.html",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber",
-                "json:target/cucumber.json"
+
+
         },
 
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
         dryRun = false,
-        tags = "@wiki",
+        tags = "",
         publish = true //generating a report with public link
 )
 public class cukesRunner {
